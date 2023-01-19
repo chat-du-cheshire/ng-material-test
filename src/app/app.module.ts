841fa3a9b77b0d3c1f41229d47b13f08c2840115
m.ivanov <m.ivanov@tinkoff.ru>
2023-01-19 20:00:31 +0300

    feature: add material

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,24 +1,21 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
 
-import { AppComponent } from './app.component';
-import {
-  HTTP_INTERCEPTORS,
-  HttpClient,
-  HttpClientModule,
-} from '@angular/common/http';
-import { FakeServerInterceptor } from './modules/fake-server/fake-server.interceptor';
+import {AppComponent} from './app.component';
+import {HTTP_INTERCEPTORS, HttpClientModule} from '@angular/common/http';
+import {FakeServerInterceptor} from './modules/fake-server/fake-server.interceptor';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 
 @NgModule({
-  declarations: [AppComponent],
-  imports: [BrowserModule, HttpClientModule],
-  providers: [
-    {
-      provide: HTTP_INTERCEPTORS,
-      multi: true,
-      useClass: FakeServerInterceptor,
-    },
-  ],
-  bootstrap: [AppComponent],
+    declarations: [AppComponent],
+    imports: [BrowserModule, HttpClientModule, BrowserAnimationsModule],
+    providers: [
+        {
+            provide: HTTP_INTERCEPTORS,
+            multi: true,
+            useClass: FakeServerInterceptor,
+        },
+    ],
+    bootstrap: [AppComponent],
 })
 export class AppModule {}
