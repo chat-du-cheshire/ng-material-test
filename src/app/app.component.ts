068411a351589a2210ef57e6dad9f92d229943b9
m.ivanov <m.ivanov@tinkoff.ru>
2023-01-19 20:40:26 +0300

    refactor: move folders upper

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,19 +1,19 @@
-import { Component } from '@angular/core';
-import { EmployeesApi } from './modules/api/employees.api';
-import { TimingsApi } from './modules/api/timings.api';
+import {Component} from '@angular/core';
+import {EmployeesApi} from './api/employees.api';
+import {TimingsApi} from './api/timings.api';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss'],
+    selector: 'app-root',
+    templateUrl: './app.component.html',
+    styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
-  title = 'ng-material-test';
-  constructor(
-    private readonly employeesApi: EmployeesApi,
-    private readonly timingsApi: TimingsApi
-  ) {
-    this.employeesApi.get().subscribe((res) => console.log('employees', res));
-    this.timingsApi.get().subscribe((res) => console.log('timings', res));
-  }
+    title = 'ng-material-test';
+    constructor(
+        private readonly employeesApi: EmployeesApi,
+        private readonly timingsApi: TimingsApi,
+    ) {
+        this.employeesApi.get().subscribe(res => console.log('employees', res));
+        this.timingsApi.get().subscribe(res => console.log('timings', res));
+    }
 }
