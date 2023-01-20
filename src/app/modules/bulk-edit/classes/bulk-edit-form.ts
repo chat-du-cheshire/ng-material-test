f2675a815f1babf6bf541efa1d47543dd6f018d6
m.ivanov <m.ivanov@tinkoff.ru>
2023-01-20 14:15:28 +0300

    feature: bulk edit form class

---
+++ src/app/modules/bulk-edit/classes/bulk-edit-form.ts
@@ -0,0 +1,48 @@
+import {FormArray, FormControl, FormGroup} from '@angular/forms';
+import {IBulkEditData, TypedGroup} from '../types/types';
+import {IEmployeeDto, ITimingDto} from '../../../types/dto';
+
+export class BulkEditForm extends FormGroup<{items: FormArray<EditForm>}> {
+    constructor(data: IBulkEditData[]) {
+        super({
+            items: new FormArray(data.map(item => new EditForm(item))),
+        });
+    }
+}
+
+export class EditForm extends FormGroup<{
+    employee: EmployeeEditForm;
+    timings: FormArray<TimingEditForm>;
+}> {
+    constructor(data: IBulkEditData) {
+        super({
+            employee: new EmployeeEditForm(data.employee),
+            timings: new FormArray(
+                data.timings.map(timing => new TimingEditForm(timing)),
+            ),
+        });
+    }
+}
+
+export class EmployeeEditForm extends FormGroup<TypedGroup<Omit<IEmployeeDto, 'email'>>> {
+    constructor(employee: IEmployeeDto) {
+        super({
+            id: new FormControl(employee.id),
+            name: new FormControl(employee.name),
+            hourlyRate: new FormControl(employee.hourlyRate),
+            hourlyRateOvertime: new FormControl(employee.hourlyRateOvertime),
+        });
+    }
+}
+
+export class TimingEditForm extends FormGroup<
+    TypedGroup<Omit<ITimingDto, 'employeeId'>>
+> {
+    constructor(timing: ITimingDto) {
+        super({
+            id: new FormControl(timing.id),
+            clockIn: new FormControl(timing.clockIn),
+            clockOut: new FormControl(timing.clockOut),
+        });
+    }
+}
